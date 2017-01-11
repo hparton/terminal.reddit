422b281be88c19a7d19f2db04181b49d0b23fc86
Harry Parton <hparton@hotmail.co.uk>
2017-01-11 15:48:46 +0000

    Add loading indicator to thread comments

--- src/components/Thread.vue
+++ src/components/Thread.vue
@@ -3,9 +3,12 @@
     <h1 class="thread__title">{{body.title}}</h1>
     <p><span class="score--positive">▲</span> {{body.score}} | submitted by {{body.author}} | {{body.created_utc | UNIXtimeAgo }}</p>
     <div class="thread__self-text" v-if="body.selftext" v-html="marked"></div>
-    <ul class="thread__comments">
+    <ul class="thread__comments" v-if="comments.length">
       <comment v-if="comments.length && comment.kind !== 'more'" v-for="comment in comments" :comment="comment.data" :depth="0"></comment>
     </ul>
+    <div v-else class="thread__comments-loading">
+      Loading Comments...
+    </div>
   </div>
 </template>
 
@@ -71,16 +74,17 @@ export default {
 
   .thread__self-text ol,
   .thread__self-text ul,
-  .comment ol,
-  .comment ul {
+  .comment-body ol,
+  .comment-body ul {
     padding-left: 20px;
   }
 
-  .thread ol {
+  .thread__self-text ol,
+  .comment-body ol {
     list-style: decimal outside;
   }
-
-  .thread ul {
+  .thread__self-text ul,
+  .comment-body ul {
     list-style: disc outside;
   }
 
